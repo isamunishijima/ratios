d4df3a75edd5f69982f8e2cfc7b6a270c1f8ca42
isamunishijima <isamu.nishijima@icloud.com>
2019-07-31 01:25:24 +0000

    error

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,6 +4,17 @@ class NumbersController < ApplicationController
   end
 
   def new
+    @number = Number.new
+  end
+  
+  def create
+    @number = Number.new(number_params)
+    if @number.save
+      redirect_to root_path, notice: "Success!"
+    else
+      flash[:alert] = "Save error!"
+      # render :new
+    end
   end
 
   def edit
@@ -11,4 +22,17 @@ class NumbersController < ApplicationController
 
   def show
   end
+  
+  private
+  
+  def number_params
+    params.require(:number).permit(:name, :current_asset, :cash, :receivable,
+                                     :inventory, :current_liability,
+                                     :payable, :total_liability, :total_debt,
+                                     :long_term_debt, :short_term_debt,
+                                     :total_equity, :sale, :EBIT,
+                                     :net_profit, :cost_of_goods_sold,
+                                     :total_asset)
+  end
+  
 end
